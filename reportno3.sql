fc27fc3e54e5f32a0fdada182817f2410875071c
trinadheddy731 <pittatrinadhreddy98@gmail.com>
2022-02-07 16:50:02 +0530

    add by reddy

--- reportno3.sql
+++ reportno3.sql
@@ -5,31 +5,31 @@ count(case s.workflow_state when "Draft" then 1 end) as "Draft",   /*Status*/
 count(case s.workflow_state when "Pending For Payment" then 1 end) as "Pending For Payment",
 count(case s.workflow_state when "Discount Approval" then 1 end) as "Discount Approval",
 count(case s.workflow_state when "AV Discount Approval" then 1 end) as "AV Discount Approval",
-count(case s.workflow_state when "Dr Discount Approval" then 1 end) as "Dr Discount Approval",
-count(case s.workflow_state when "Address Verification" then 1 end) as "Address Verification",
-count(case s.workflow_state when "AV Hold" then 1 end) as "AV Hold",
-count(case s.workflow_state when "Dr. Advice" then 1 end) as "Dr. Advice",
-count(case s.workflow_state when "Dispatch" then 1 end) as "Dispatch",
-count(case s.workflow_state when "Rejected" then 1 end) as "Rejected",
-count(case s.workflow_state when "Delivered" then 1 end) as "Delivered",
-count(case s.workflow_state when "Cancelled" then 1 end) as "Cancelled",
-count(case s.workflow_state when "Returned" then 1 end) as "Returned",
-count(case s.workflow_state when "Out for Pickup" then 1 end) as "Out for Pickup",
-(case when s.workflow_state != "Pincode Not Serviceable"
-            and s.workflow_state != "On Hold"
-            and s.workflow_state != "To Deliver and Bill"
-            and s.workflow_state != "To Bill"
-            and s.workflow_state != "To Deliver"
-            and s.workflow_state != "Urgent Delivery" 
-            and s.workflow_state != "Closed"
-            and s.workflow_state != "Re Attempt" 
-            and s.workflow_state != "Shipped" 
-            and s.workflow_state != "Verification Pending"  then 1 end ) as "Othernnbhbs",
+count(case s.status when "Dr Discount Approval" then 1 end) as "Dr Discount Approval",
+count(case s.status when "Address Verification" then 1 end) as "Address Verification",
+count(case s.status when "AV Hold" then 1 end) as "AV Hold",
+count(case s.status when "Dr. Advice" then 1 end) as "Dr. Advice",
+count(case s.status when "Dispatch" then 1 end) as "Dispatch",
+count(case s.status when "Rejected" then 1 end) as "Rejected",
+count(case s.status when "Delivered" then 1 end) as "Delivered",
+count(case s.status when "Cancelled" then 1 end) as "Cancelled",
+count(case s.status when "Returned" then 1 end) as "Returned",
+count(case s.status when "Out for Pickup" then 1 end) as "Out for Pickup",
+
+
+count(case when s.status != "Pincode Not Serviceable"
+            and s.status != "On Hold"
+            and s.status != "To Deliver and Bill"
+            and s.status != "To Bill"
+            and s.status != "To Deliver"
+            and s.status != "Urgent Delivery" 
+            and s.status != "Closed"
+            and s.status != "Re Attempt" 
+            and s.status != "Shipped" 
+            and s.status != "Verification Pending"  then 1 end ) as "Other",
 round(((count(case s.workflow_state when "Delivered" then 1 end)/count(name))*100),2 )as "Delivered Percentage(After AV)"
 /*if(count(case s.workflow_state when "Delivered" then 1 end)/(count(name)-(count(case workflow_state when "Address Verification" then 1 end)+count(case workflow_state when "Rejected" then 1 end)))*100,count(case s.workflow_state when "Delivered" then 1 end)/(count(name)-(count(case workflow_state when "Address Verification" then 1 end)+count(case workflow_state when "Rejected" then 1 end)))*100,0) as "Delivered Percentage" */
 from 
-(select so.workflow_state as "workflow_state", so.name as "name", so.call_center as "call_center", so.transaction_date as "transaction_date"
-from
-`tabSales Order` so ) as s
-
+(select status,name,call_center,transaction_date from
+`tabSales Order` ) as s
 group by s.call_center
